a1be31eb9a1f2eabd85d7439a280f7d9c9dc6abd
JoshZ <76496304+jjjoshzhang@users.noreply.github.com>
2025-07-04 09:36:26 +0800

    Delete learn1_tipcalculator.py

--- learn1_tipcalculator.py
+++
@@ -1,7 +0,0 @@
-print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip = int(input("What percentage tip would you like to give? 10 12 15 "))
-people = int(input("How many people to split the bill? "))
-amount = float((bill / people) * (1+tip/100))
-print("Each person should pay: $" + str(round(amount,2)))
-
