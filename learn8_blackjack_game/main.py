8f59e75d021ca54f66f3328bafb228676ad03a8f
JoshZ <76496304+jjjoshzhang@users.noreply.github.com>
2025-07-25 23:24:06 +0800

    Update main.py

--- learn8_blackjack_game/main.py
+++ learn8_blackjack_game/main.py
@@ -1,1 +1,41 @@
+import art
+import random
 
+
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+y_or_no = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
+if y_or_no == 'y':
+    print("\n"*20)
+    print(art.logo)
+    score = 0
+    score_c =0
+    human_numbers = random.sample(cards,2)
+    for num in human_numbers:
+            score += num
+    print(f"Your cards: {human_numbers}, current score: {score}")
+    com_numbers = random.sample(cards,2)
+    print(f"Computer's first card is: {com_numbers[0]}")
+    y_or_no = input("Type 'y' to get another card, type 'n' to pass.").lower()
+    if y_or_no == 'y':
+        human_numbers += random.sample(cards,1)
+        score = sum(human_numbers)
+        score_c = sum(com_numbers)
+        print(f"Your cards: {human_numbers}, final score: {score}")
+        print(f"Computer's final hand is :{com_numbers}, final score: {score_c}")
+        if score < score_c and score < 22:
+            print("You win")
+        elif score_c < score and score_c < 22:
+            print("Computer win")
+
+    else:
+        com_numbers += random.sample(cards,1)
+        score_c = sum(com_numbers)
+        print(f"Your cards: {human_numbers}, final score: {score}")
+        print(f"Computer's final hand is :{com_numbers}, final score: {score_c}")
+        if score < score_c and score < 22:
+            print("You win")
+        elif score_c < score and score_c < 22:
+            print("Computer win")
+else:
+    quit()
